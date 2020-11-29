0860f2c412d0d2af7946730ebb7f1b4b27b8604e
Swapnali Jadhav <swapnalij4050@gmail.com>
2020-11-29 18:11:38 +0530

    First Jenkins comment

---
+++ src/test/java/Test/Jenkis/Test1.java
@@ -0,0 +1,17 @@
+package Test.Jenkis;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+	WebDriver	 driver = new ChromeDriver();
+	driver.get("www.google.com");
+	}
+
+	}
